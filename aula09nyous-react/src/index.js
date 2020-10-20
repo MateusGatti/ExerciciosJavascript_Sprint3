539b5f53c33047f882ec64b88e36a9b052d0fc18
Mateus Mello <mt.gatti@hotmail.com>
2020-10-20 17:34:55 -0300

    Aula 10 finalizada

--- aula09nyous-react/src/index.js
+++ aula09nyous-react/src/index.js
@@ -5,6 +5,7 @@ import * as serviceWorker from './serviceWorker';
 import Home from './pages/home';
 import Login from './pages/login';
 import Cadastrar from './pages/cadastrar';
+import Eventos from './pages/eventos';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
@@ -14,6 +15,7 @@ const routing = (
       <Route exact path='/' component={Home} />
       <Route path='/login' component={Login} />
       <Route path='/cadastrar' component={Cadastrar} />
+      <Route path='/eventos' component={Eventos} />
     </Switch>
   </Router>
 )
--- aula09nyous-react/src/pages/login/index.jsx
+++ aula09nyous-react/src/pages/login/index.jsx
@@ -1,15 +1,50 @@
-import React from 'react';
+import React, { useState,  } from 'react';
+import { useHistory } from "react-router-dom";
 import Menu from '../../components/menu';
 import Rodape from '../../components/rodape';
 import {Container, Form, Button} from 'react-bootstrap';
 import './index.css';
 
 const Login = () => {
+
+    let history = useHistory();
+
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+
+    const logar = (event) => {
+        event.preventDefault();
+
+        fetch('http://localhost:5000/api/account/login', {
+            method : 'POST',
+            body : JSON.stringfy({
+                email : email,
+                senha : senha
+            }),
+            headers : {
+                'content-type' : 'application/json'
+            }
+        })
+        .then(response => {
+            if(response.ok){
+                return response.json();
+            }
+
+            alert('Dados inválidos');
+        })
+        .then(data => {
+            localStorage.setItem('token-nyous-tarde', data.token);
+            history.push("/eventos");
+        })
+        .catch(err => console.error(err));
+    }
+
+
     return (
         <div>
         <Menu />
         <Container className='form-height'>
-                <Form className='form-signin' >
+                <Form className='form-signin' onSubmit={ event => logar(event)} >
                     <div className='text-center'>
                     </div>
                     <br/>
@@ -17,12 +52,12 @@ const Login = () => {
                     <hr/>
                     <Form.Group controlId="formBasicEmail">
                         <Form.Label>Email </Form.Label>
-                        <Form.Control type="email" placeholder="Informe o email" required />
+                        <Form.Control type="email" value={email} onChange={ event => setEmail(event.target.value)} placeholder="Informe o email" required />
                     </Form.Group>
 
                     <Form.Group controlId="formBasicPassword">
                         <Form.Label>Senha</Form.Label>
-                        <Form.Control type="password" placeholder="Senha"  required/>
+                        <Form.Control type="password" value={senha} onChange={ event => setSenha(event.target.value)} placeholder="Senha"  required/>
                     </Form.Group>
                     <Button variant="primary" type="submit">
                         Enviar
